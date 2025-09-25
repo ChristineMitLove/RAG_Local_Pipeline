764922460379f28c5e64de772c464611a62d5fd1
mitxiao <wangthujulia@google.com>
2025-09-25 17:47:05 +0800

    add url format check & requirements.txt

--- rag_local_pipeline.py
+++ rag_local_pipeline.py
@@ -3,28 +3,32 @@ import os
 import requests
 
 # Get PDF document
-pdf_path = "human-nutrition-text.pdf"
+pdf_ = "human-nutrition-text.pdf"
 
 # Download PDF if it doesn't already exist
 if not os.path.exists(pdf_path):
-  print("File doesn't exist, downloading...")
+    print("File doesn't exist, downloading...")
 
-  
-  url = "https://pressbooks.oer.hawaii.edu/humannutrition2/open/download?type=pdf"
+    url = "https://pressbooks.oer.hawaii.edu/humannutrition2/open/download?type=pdf"
 
-  
-  filename = pdf_path
-
-  # Send a GET request to the URL
-  response = requests.get(url)
-
-  # Check if the request was successful
-  if response.status_code == 200:
-      # Open a file in binary write mode and save the content to it
-      with open(filename, "wb") as file:
-          file.write(response.content)
-      print(f"The file has been downloaded and saved as {filename}")
-  else:
-      print(f"Failed to download the file. Status code: {response.status_code}")
+    response = requests.get(url)
+    if response.headers.get('Content-Type') == text/html:
+        print('Your url is of text/html format. Update a correct PDF url please.')
+    else:
+        filename = pdf_path
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+        # Open a file in binary write mode and save the content to it
+            with open(filename, "wb") as file:
+                file.write(response.content)
+                print(f"The file has been downloaded and saved as {filename}")
+        else:
+            print(f"Failed to download the file. Status code: {response.status_code}")
 else:
-  print(f"File {pdf_path} exists.")
+    print(f"File {pdf_path} exists.")
+
+
+
+  
+  
